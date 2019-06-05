f55d42a75266abe526b889959dc6b40f8eadad9a
Caio Matheus Campos de Oliveira <caiomcoliveira@gmail.com>
2019-06-05 17:07:10 -0300

    Fixedi s attom

--- app/Http/Controllers/ResultsController.php
+++ app/Http/Controllers/ResultsController.php
@@ -22,7 +22,7 @@ class ResultsController extends Controller
                     $linhas[$counter][0] = $subject->square->id;
                     $linhas[$counter][1] = $subject->id;
                     $linhas[$counter][2] = $question->id;
-                    $linhas[$counter][3] = $question->isAtom ? "YES" : "NO";
+                    $linhas[$counter][3] = $question->is_atom ? "YES" : "NO";
                     $linhas[$counter][4] = $question->pivot->subject_time;
                     $correct = strtolower($question->answer) == strtolower($question->pivot->subject_answer) ? "CORRECT" : "WRONG";
                     $linhas[$counter][5] = $correct;
